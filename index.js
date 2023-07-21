6579e8fc2ea3bfe1179d7e5475357352ebcc575d
Amarmanhar <58129626+Amarmanhar@users.noreply.github.com>
2023-07-21 16:29:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const sequelize = require('./util/database');
-const Products = require('./models/product');
-
+const productRouter = require('./router/product');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,45 +11,15 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) =>{
-    res.send('hello baby');
+    res.send('hello from server');
 })
 
-app.post('/add-product', async(req, res)=>{
-      
-    const name = req.body.name;
-    const price = req.body.price;
-    
-    const data = await Products.create({
-        name: name,
-        price:price,
-    })
-    res.status(200).json({allproduct: data});
+app.use('/product', productRouter);
 
-})
+app.use('/get', productRouter);
 
-app.get('/get-product', (req, res) => {
-    try{
-        Products.findAll()
-      .then((users) => {
-        console.log(users);
-        res.status(200).json({ allproducts: users });
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-      })
-    }
-    catch{
-          console.log('error');
-    }     
-  });
+app.use('/delete-product', productRouter);
 
-  app.delete('/delete-product/:id', async(req, res) =>{
-    
-    const Id = req.params.id;
-    await Products.destroy({where: {id:Id}});
-    res.status(200);
-  })
 
 sequelize.sync()
 .then((result)=>{
@@ -61,3 +30,4 @@ sequelize.sync()
 .catch((error)=>{
   console.log(error);
 })
+
