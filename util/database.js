71f2d490d131061decd55697267729fd3c37dd13
Amarmanhar <58129626+Amarmanhar@users.noreply.github.com>
2023-07-21 16:26:53 +0530

    Delete util directory

--- util/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('productdata', 'root', 'Amar@123',{
-    dialect: 'mysql',
-    host:'localhost',
-});
-
-
-module.exports = sequelize;
